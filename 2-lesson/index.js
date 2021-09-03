a503e24cde1c55ba0535424485db4aceb4569bf5
Andrey2191 <oxido9055@gmail.com>
2021-09-03 14:09:21 +0300

    add task

--- 2-lesson/index.js
+++ 2-lesson/index.js
@@ -26,21 +26,21 @@ const p1 = new Promise((resolve, reject) => {
     setTimeout(() => {
         console.log('1');
         resolve()
-    },Math.floor(Math.random() * 5000))
+    }, Math.floor(Math.random() * 5000))
 })
 
 const p2 = new Promise((resolve, reject) => {
     setTimeout(() => {
         console.log('2');
         resolve()
-    },Math.floor(Math.random() * 5000))
+    }, Math.floor(Math.random() * 5000))
 })
 
 const p3 = new Promise((resolve, reject) => {
     setTimeout(() => {
         console.log('3');
         resolve()
-    },Math.floor(Math.random() * 5000))
+    }, Math.floor(Math.random() * 5000))
 })
 
 Promise.all([p1, p2, p3]).then(() => {
@@ -61,7 +61,7 @@ fetch(url)
 const cards = document.getElementById(1)
 
 function printCard(obj) {
-    let {name, username, phone, email, website} = obj
+    let { name, username, phone, email, website } = obj
     let userCard = document.createElement('div')
     userCard.append(`Name: ${name}, Username: ${username}, Phone: ${phone}, E-mail: ${email}, Website: ${website}`)
     cards.append(userCard)
@@ -70,41 +70,68 @@ function printCard(obj) {
 const url = ('https://jsonplaceholder.typicode.com/users')
 function render() {
     fetch(url)
-    .then(response => response.json())
-    .then(result => {
-        result.forEach(i => printCard(i))
-    })
+        .then(response => response.json())
+        .then(result => {
+            result.forEach(i => printCard(i))
+        })
 }
 
 render()
 
 
 // 6 задача
+async function getUsers() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users")
+    const result = await res.json()
+    console.log('user', result)
+
+    result.forEach((user) => {
+        const { name, email } = user;
+        const postElement = renderUser(name, email);
+        document.body.append(postElement)
+    });
+}
+getUsers()
+
+function renderUser(name, email) {
+    let container = document.createElement('div')
+    const titleElement = document.createElement('h2')
+    titleElement.innerHTML = name
+
+    const bodyElement = document.createElement('p')
+    bodyElement.innerHTML = email
+
+    container.append(titleElement, bodyElement)
+    return container;
+}
+
+renderUser()
+
 
 // 7 задача
 let container = document.getElementById('3')
 
-function createImg(url){
+function createImg(url) {
     let img = document.createElement('img')
     img.className = 'img'
     img.src = url
     container.append(img)
 }
 
-function getImg(){
+function getImg() {
     fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
-    .then(response => response.json())
-    .then(result => {
-        result.forEach(element => {
-            createImg(element.thumbnailUrl)
-        });
-        container.addEventListener('click', (e) => {
-            let {target} = e 
-            if(target.classList.contains('img')){
-                target.classList.toggle('zoom')
-            }
+        .then(response => response.json())
+        .then(result => {
+            result.forEach(element => {
+                createImg(element.thumbnailUrl)
+            });
+            container.addEventListener('click', (e) => {
+                let { target } = e
+                if (target.classList.contains('img')) {
+                    target.classList.toggle('zoom')
+                }
+            })
         })
-    })
 }
-       
+
 getImg()
